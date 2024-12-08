988c20a65b20b777ec62cf255148779f0530dc78
happysulla <happysulla@gmail.com>
2024-12-08 08:14:56 -0500

    when loading game, show coins and wit-wiles in status bar

--- BarbarianPrinceGame/View/StatusBarViewer.cs
+++ BarbarianPrinceGame/View/StatusBarViewer.cs
@@ -32,21 +32,27 @@ namespace BarbarianPrince
       //-----------------------------------------------------------------
       public void UpdateView(ref IGameInstance gi, GameAction action)
       {
-         if ((GameAction.UpdateLoadingGame == action) || (GameAction.UpdateNewGame == action))
+         if (GameAction.UpdateNewGame == action)
          {
             myGameInstance = gi;
-         }
-         if (false == myIsCoinShown) // If autostart option is selected, show the party's coin and Wits and Wiles images
-         {
-            Option option = gi.Options.Find("AutoSetup");
-            if (null != option)
+            if (false == myIsCoinShown) // If autostart option is selected, show the party's coin and Wits and Wiles images
             {
-               if (true == option.IsEnabled)
+               Option option = gi.Options.Find("AutoSetup");
+               if (null != option)
                {
-                  myIsCoinShown = true;
-                  myIsWitAndWilesShown = true;
+                  if (true == option.IsEnabled)
+                  {
+                     myIsCoinShown = true;
+                     myIsWitAndWilesShown = true;
+                  }
                }
             }
+         }
+         if (GameAction.UpdateLoadingGame == action)
+         {
+            myGameInstance = gi;
+            myIsCoinShown = true;
+            myIsWitAndWilesShown = true;
          }
          //-------------------------------------------------------
          if ((null != myTargetCursor) && (GameAction.UpdateStatusBar == action)) // increase/decrease size of cursor when zoom in or out
