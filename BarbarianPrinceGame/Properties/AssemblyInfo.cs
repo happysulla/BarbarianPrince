54bd8bc86af179f0e93f3fb5b41e0fc8726fb659
happysulla <happysulla@gmail.com>
2024-12-15 20:06:35 -0500

    Release 2.1.17

--- BarbarianPrinceGame/Properties/AssemblyInfo.cs
+++ BarbarianPrinceGame/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.16")]
-[assembly: AssemblyFileVersion("2.0.16")]
+[assembly: AssemblyVersion("2.1.17")]
+[assembly: AssemblyFileVersion("2.1.17")]
 [assembly: Guid("78F8744A-EB40-4E18-9449-B1A7B09C402D")]
